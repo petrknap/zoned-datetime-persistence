73d8cb346008fb0fd4be162a62eaa958fef122ef
Petr Knap <8299754+petrknap@users.noreply.github.com>
2025-11-05 21:09:56 +0100

    chore: added maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,27 @@
+import java.io.ByteArrayOutputStream
+
+val gitTag = ByteArrayOutputStream()
+exec {
+    commandLine = listOf("git", "describe", "--tags", "--abbrev=0")
+    isIgnoreExitValue = true
+    standardOutput = gitTag
+}
+val gitBranch = ByteArrayOutputStream()
+exec {
+    commandLine = listOf("git", "rev-parse", "--abbrev-ref", "HEAD")
+    standardOutput = gitBranch
+}
+
+description = "Timezone aware date-time persistence"
+group = "io.github.petrknap"
+version = gitTag.toString().trim().removePrefix("v")
+if (version == "") {
+    version = (gitBranch.toString().trim() + "-SNAPSHOT").removePrefix("-")
+}
+
 plugins {
     `java-library`
+    `maven-publish`
 }
 
 repositories {
@@ -23,4 +45,31 @@ java {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                url = "https://github.com/petrknap/zoned-datetime-persistence"
+                developers {
+                    val authorsFile = file("AUTHORS")
+                    if (authorsFile.exists()) {
+                        authorsFile.readLines().forEach { line ->
+                            val regex = Regex("(.*) <(.*)>")
+                            val match = regex.matchEntire(line)
+                            if (match != null) {
+                                developer {
+                                    name.set(match.groupValues[1].trim())
+                                    email.set(match.groupValues[2].trim())
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "zoneddatetime-persistence"
+rootProject.name = "zoned-datetime-persistence"
 include("lib")
